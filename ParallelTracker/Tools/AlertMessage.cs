8e18c10b050ea66890e830222603632881cbc541
Daniel <danlpsarcanjo@gmail.com>
2021-01-12 00:16:22 -0500

    Add alert handling in _Layout page

---
+++ ParallelTracker/Tools/AlertMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParallelTracker.Tools
+{
+    public class AlertMessage
+    {
+        public AlertMessage(AlertMessageType type, string text)
+        {
+            Type = type;
+            Text = text;
+        }
+
+        public AlertMessageType Type { get; private set;  }
+        public string Text { get; private set; }
+    }
+
+    public enum AlertMessageType
+    {
+        None,
+        Success,
+        Error,
+        Info,
+        Warnig
+    }
+}
