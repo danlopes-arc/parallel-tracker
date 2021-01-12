5187676d0ed0d93e015a09fd59dd90c2d1346465
Daniel <danlpsarcanjo@gmail.com>
2021-01-12 00:53:53 -0500

    Modify alert hadling

---
+++ ParallelTracker/Tools/AlertMessageType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParallelTracker.Tools
+{
+    public static class AlertMessageType
+    {
+        public const string Success = "success";
+        public const string Danger = "danger";
+        public const string Warning = "warning";
+        public const string Info = "info";
+    }
+}
