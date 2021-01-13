f7604c05acc65404bb2a3c4c70293686e4fe8dd4
Daniel <danlpsarcanjo@gmail.com>
2021-01-13 16:34:27 -0500

    Create Migration

---
+++ ParallelTracker/Migrations/20210113195652_AddRelationships.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParallelTracker.Migrations
+{
+    public partial class AddRelationships : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RepoId",
+                table: "Issues",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_RepoId",
+                table: "Issues",
+                column: "RepoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Issues_Repos_RepoId",
+                table: "Issues",
+                column: "RepoId",
+                principalTable: "Repos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Issues_Repos_RepoId",
+                table: "Issues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Issues_RepoId",
+                table: "Issues");
+
+            migrationBuilder.DropColumn(
+                name: "RepoId",
+                table: "Issues");
+        }
+    }
+}
