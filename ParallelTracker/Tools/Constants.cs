b5603615b8a2e991c935a963150ed9f526caa34b
Daniel <danlpsarcanjo@gmail.com>
2021-01-12 21:14:35 -0500

    Create Constants class

---
+++ ParallelTracker/Tools/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParallelTracker.Tools
+{
+    public static class Constants
+    {
+        public const string AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
+        public const string AllowedUserNameCharactersPattern = @"^[a-zA-Z0-9\-_]+$";
+    }
+}
