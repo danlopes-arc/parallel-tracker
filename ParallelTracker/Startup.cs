5f3ef66c768c17681bddb87f3d38a677079c092e
Daniel <danlpsarcanjo@gmail.com>
2021-01-15 23:20:55 -0500

    Set Repos as default controller

--- ParallelTracker/Startup.cs
+++ ParallelTracker/Startup.cs
@@ -92,7 +92,7 @@ namespace ParallelTracker
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Repos}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
